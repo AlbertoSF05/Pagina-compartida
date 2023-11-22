266f930f586f3582294ae8b795339f54d37827b2
AlbertoSF05 <150022452+AlbertoSF05@users.noreply.github.com>
2023-11-22 14:26:56 +0100

    Add files via upload

---
+++ Pagina-junta/Alberto/JSS01/java.js
@@ -0,0 +1,99 @@
+function funcionQuienessomos() {
+    var x = document.getElementById("Quienessomos");
+    var y = document.getElementById("Obranueva");
+    var a = document.getElementById("Arquitectura");
+    x.style.display = "block";
+    y.style.display = "none";
+    a.style.display = "none";
+
+}
+
+function funcionObranueva() {
+    var x = document.getElementById("Quienessomos");
+    var y = document.getElementById("Obranueva");
+    var a = document.getElementById("Arquitectura");
+    x.style.display = "none";
+    y.style.display = "block";
+    a.style.display = "none";
+}
+
+function funcionArquitectura() {
+    var x = document.getElementById("Quienessomos");
+    var y = document.getElementById("Obranueva");
+    var a = document.getElementById("Arquitectura");
+    x.style.display = "none";
+    y.style.display = "none";
+    a.style.display = "block";
+}
+
+
+
+
+
+/*
+function funcionQuienessomos() {
+    var x = document.getElementById("Quienessomos");
+    var y = document.getElementById("Obranueva");
+    var z = document.getElementById("Reformas");
+    var a = document.getElementById("Arquitectura");
+    var b = document.getElementById("Proyectos");
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "none";
+}
+
+
+function funcionObranueva() {
+    var x = document.getElementById("Quienessomos");
+    var y = document.getElementById("Obranueva");
+    var z = document.getElementById("Reformas");
+    var a = document.getElementById("Arquitectura");
+    var b = document.getElementById("Proyectos");
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "none";
+}
+
+function funcionReformas() {
+    var x = document.getElementById("Quienessomos");
+    var y = document.getElementById("Obranueva");
+    var z = document.getElementById("Reformas");
+    var a = document.getElementById("Arquitectura");
+    var b = document.getElementById("Proyectos");
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "block";
+    a.style.display = "none";
+    b.style.display = "none";
+}
+
+function funcionArquitectura() {
+    var x = document.getElementById("Quienessomos");
+    var y = document.getElementById("Obranueva");
+    var z = document.getElementById("Reformas");
+    var a = document.getElementById("Arquitectura");
+    var b = document.getElementById("Proyectos");
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "none";
+    a.style.display = "block";
+    b.style.display = "none";
+}
+
+function funcionProyectos() {
+    var x = document.getElementById("Quienessomos");
+    var y = document.getElementById("Obranueva");
+    var z = document.getElementById("Reformas");
+    var a = document.getElementById("Arquitectura");
+    var b = document.getElementById("Proyectos");
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "block";
+}
+*/
